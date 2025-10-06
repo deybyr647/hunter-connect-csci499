7c18613ed036749ec322d62e73d6cf6d2b17ab1b
deybyr647 <deybyr647@gmail.com>
2025-10-06 13:54:54 -0400

    Restructured project to separate frontend & backend

---
+++ hunter-connect-be/src/main/java/com/hunter_connect/demo/HunterConnectBeApplication.java
@@ -0,0 +1,13 @@
+package com.hunter_connect.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunterConnectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HunterConnectBeApplication.class, args);
+	}
+
+}
---
+++ hunter-connect-be/src/test/java/com/hunter_connect/demo/HunterConnectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunter_connect.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HunterConnectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
