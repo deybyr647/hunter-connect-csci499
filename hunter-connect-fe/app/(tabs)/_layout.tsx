969d01f4f5bfa752e95b6002fb9e70ec87bf81cd
deybyr647 <deybyr647@gmail.com>
2025-10-09 01:10:32 -0400

    minor content changes

--- hunter-connect-fe/app/(tabs)/_layout.tsx
+++ hunter-connect-fe/app/(tabs)/_layout.tsx
@@ -32,7 +32,7 @@ export default function TabLayout() {
         <Tabs.Screen
             name="Landing"
             options={{
-                title: 'Landing',
+                title: 'Home',
             }}
         />
 
