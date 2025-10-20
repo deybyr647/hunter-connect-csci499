5ec0338b42202ca1bb3c965c2d9a90c5f046a6f2
CrazyGenie <stevenlai551@gmail.com>
2025-10-19 23:42:14 -0400

    Setup firebase with working signup/login pages

---
+++ hunter-connect-fe/firebase/firebaseConfig.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADhQn9EHEt3N5OmMMpm0N3ccjr-HyfjOA",
+  authDomain: "hunterconnect-80634.firebaseapp.com",
+  projectId: "hunterconnect-80634",
+  storageBucket: "hunterconnect-80634.firebasestorage.app",
+  messagingSenderId: "555459134033",
+  appId: "1:555459134033:web:24a45068ba72aa0081ae70",
+  measurementId: "G-8N2YR7GFQ3"
+};
+
+// ✅ Initialize Firebase first
+const app = initializeApp(firebaseConfig);
+
+// ✅ Initialize services after app
+const auth = getAuth(app);
+
+// export app in case we need it elsewhere
+export { app, auth };
