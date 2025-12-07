68532cb358a8f1b70ec237efd401f571b9c2119e
deybyr647 <deybyr647@gmail.com>
2025-12-07 04:15:54 -0500

    added front-end code for creating backend calls related to Events and made style changes to message page

---
+++ hunter-connect-fe/app/(messages)/_layout.tsx
@@ -0,0 +1,21 @@
+import { Stack } from "expo-router";
+import TopHeader from "@/components/TopHeader";
+
+export default function MessagesLayout() {
+    return (
+        <Stack
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen
+                name="messages"
+                options={{
+                    title: "Messages",
+                    header: () => <TopHeader />,
+                    headerShown: true
+                }}
+            />
+        </Stack>
+    );
+}
